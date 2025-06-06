d010c052f06d1f6eabd18562798f32d202ed9501
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:28 +0000

    [Sync Iteration] cpp/food-chain/3

---
+++ solutions/cpp/food-chain/3/food_chain.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace food_chain {
+std::string verse(int verse_number);
+std::string verses(int start_verse, int end_verse);
+std::string sing();
+
+}  // namespace food_chain
