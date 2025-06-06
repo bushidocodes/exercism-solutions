6c74ad0faa52ad0d1c2f1de17fc40a72b18a5f95
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:16 +0000

    [Sync Iteration] rust/high-scores/1

---
+++ solutions/rust/high-scores/1/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.iter().last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut buffer = self.scores.to_vec();
+        buffer.sort_unstable();
+        buffer.reverse();
+        if buffer.len() > 3 {
+            buffer.resize(3, 0);
+        }
+        buffer
+    }
+}
