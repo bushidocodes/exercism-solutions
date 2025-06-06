015b1d8188f07a1ff088b2f07f13032236f3b30d
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:36 +0000

    [Sync Iteration] cpp/triangle/3

---
+++ solutions/cpp/triangle/3/triangle.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace triangle {
+enum flavor { equilateral, isosceles, scalene };
+
+enum flavor kind(double a_length, double b_length, double c_length);
+
+}  // namespace triangle
