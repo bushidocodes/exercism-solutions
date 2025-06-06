880b35b3498730a0263bd35669493d69244f7bf6
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:48 +0000

    [Sync Iteration] cpp/allergies/2

---
+++ solutions/cpp/allergies/2/allergies.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <bitset>
+#include <cstdint>
+#include <unordered_set>
+
+namespace allergies {
+
+class allergy_test {
+   private:
+	std::bitset<32> allergies;
+
+   public:
+	allergy_test(std::uint32_t score);
+	bool is_allergic_to(const std::string &allergy);
+	std::unordered_set<std::string> get_allergies();
+};
+
+}  // namespace allergies
