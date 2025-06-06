6590762112c871480f9ffd5cfc936566f171f9d2
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:56 +0000

    [Sync Iteration] cpp/robot-simulator/3

---
+++ solutions/cpp/robot-simulator/3/robot_simulator.cpp
@@ -0,0 +1,77 @@
+#include "robot_simulator.h"
+
+namespace robot_simulator {
+Robot::Robot(position_t position, enum Bearing bearing)
+    : position{position}, bearing{bearing} {}
+const position_t Robot::get_position() const { return position; }
+
+enum Bearing Robot::get_bearing() const { return bearing; }
+
+void Robot::turn_right() {
+	switch (bearing) {
+		case Bearing::NORTH:
+			bearing = Bearing::EAST;
+			break;
+		case Bearing::EAST:
+			bearing = Bearing::SOUTH;
+			break;
+		case Bearing::SOUTH:
+			bearing = Bearing::WEST;
+			break;
+		case Bearing::WEST:
+			bearing = Bearing::NORTH;
+			break;
+	}
+}
+
+void Robot::turn_left() {
+	switch (bearing) {
+		case Bearing::NORTH:
+			bearing = Bearing::WEST;
+			break;
+		case Bearing::WEST:
+			bearing = Bearing::SOUTH;
+			break;
+		case Bearing::SOUTH:
+			bearing = Bearing::EAST;
+			break;
+		case Bearing::EAST:
+			bearing = Bearing::NORTH;
+			break;
+	}
+}
+
+void Robot::advance() {
+	switch (bearing) {
+		case Bearing::NORTH:
+			position.second++;
+			break;
+		case Bearing::WEST:
+			position.first--;
+			break;
+		case Bearing::SOUTH:
+			position.second--;
+			break;
+		case Bearing::EAST:
+			position.first++;
+			break;
+	}
+}
+
+void Robot::execute_sequence(const std::string &sequence) {
+	for (char step : sequence) {
+		switch (step) {
+			case 'R':
+				this->turn_right();
+				break;
+			case 'L':
+				this->turn_left();
+				break;
+			case 'A':
+				this->advance();
+				break;
+		}
+	}
+}
+
+}  // namespace robot_simulator
---
+++ solutions/cpp/robot-simulator/3/robot_simulator.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <utility>
+
+namespace robot_simulator {
+using position_t = std::pair<int, int>;
+
+enum Bearing { SOUTH, NORTH, EAST, WEST };
+
+class Robot {
+   private:
+	position_t position;
+	enum Bearing bearing;
+
+   public:
+	Robot(position_t position = {0, 0}, enum Bearing = Bearing::NORTH);
+	const position_t get_position() const;
+	enum Bearing get_bearing() const;
+	void turn_right();
+	void turn_left();
+	void advance();
+	void execute_sequence(const std::string &sequence);
+};
+
+}  // namespace robot_simulator
