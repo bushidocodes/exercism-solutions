3a37371c21952b9e26dcffa503241a9a6a3f19fb
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:50 +0000

    [Sync Iteration] cpp/matching-brackets/1

---
+++ solutions/cpp/matching-brackets/1/matching_brackets.cpp
@@ -0,0 +1,44 @@
+#include "matching_brackets.h"
+
+#include <stack>
+#include <map>
+
+namespace matching_brackets
+{
+    std::map<char, char> bracket_pairs{
+        {']', '['},
+        {')', '('},
+        {'}', '{'}};
+
+    bool check(std::string to_check)
+    {
+        std::stack<char> brackets{};
+
+        for (char c : to_check)
+        {
+            switch (c)
+            {
+            case '[':
+            case '(':
+            case '{':
+                /* If an opening bracket, add to the stack */
+                brackets.push(c);
+                break;
+            case ']':
+            case ')':
+            case '}':
+                /* If closing bracket, confirm corresponding opening bracket on top of stack */
+                if (brackets.empty() || brackets.top() != bracket_pairs.at(c))
+                {
+                    return false;
+                }
+                brackets.pop();
+                break;
+            }
+        }
+
+        /* Check that all opening brackets were terminated properly */
+        return brackets.empty();
+    }
+
+} // namespace matching_brackets
---
+++ solutions/cpp/matching-brackets/1/matching_brackets.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace matching_brackets
+{
+    bool check(std::string to_check);
+
+} // namespace matching_brackets
