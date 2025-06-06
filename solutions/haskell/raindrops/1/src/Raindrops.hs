b845a0cf6fc0180ea74e8f1ba5f44078487f7c44
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:53:54 +0000

    [Sync Iteration] haskell/raindrops/1

---
+++ solutions/haskell/raindrops/1/src/Raindrops.hs
@@ -0,0 +1,8 @@
+module Raindrops (convert) where
+
+convert :: Int -> String
+convert n = do
+  let sounds = ((if n `mod` 3  == 0 then "Pling" else "") ++ 
+                (if n `mod` 5  == 0 then "Plang" else "") ++ 
+                (if n `mod` 7  == 0 then "Plong" else ""))
+  if sounds /= "" then sounds else show n
