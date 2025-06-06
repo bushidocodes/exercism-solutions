5329029275b9850ae20a9bed4023551741dcc24d
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:48 +0000

    [Sync Iteration] cpp/queen-attack/2

---
+++ solutions/cpp/queen-attack/2/queen_attack.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <utility>
+
+namespace queen_attack {
+using pos_t = std::pair<int, int>;
+
+class chess_board {
+   private:
+	pos_t _white;
+	pos_t _black;
+
+   public:
+	chess_board(pos_t white = {0, 3}, pos_t black = {7, 3});
+	pos_t white() const;
+	pos_t black() const;
+	bool can_attack() const;
+	operator std::string() const;
+};
+
+}  // namespace queen_attack
