85bc25ea0a51ab46500deab61e90b298f08e3832
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:40 +0000

    [Sync Iteration] cpp/binary/1

---
+++ solutions/cpp/binary/1/binary.cpp
@@ -0,0 +1,25 @@
+#include "binary.h"
+#include <bitset>
+
+namespace binary
+{
+    uint32_t convert(std::string binary)
+    {
+        std::bitset<64> buffer;
+        size_t binary_length = binary.length() - 1;
+        for (size_t i = 0; i <= binary_length; i++)
+        {
+            if (binary[binary_length - i] == '1')
+            {
+                buffer[i] = true;
+            }
+            else if (binary[binary_length - i] != '0')
+            {
+                /* Character was neither 0 nor 1, so return 0 to signify invalid */
+                return 0;
+            }
+        }
+        return buffer.to_ulong();
+    }
+
+} // namespace binary
---
+++ solutions/cpp/binary/1/binary.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace binary
+{
+    uint32_t convert(std::string binary);
+} // namespace binary
