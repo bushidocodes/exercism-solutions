e2285f65eb19c3142520f54bcbf506ad4bc3c151
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:46 +0000

    [Sync Iteration] c/saddle-points/5

---
+++ solutions/c/saddle-points/5/src/saddle_points.h
@@ -0,0 +1,22 @@
+#ifndef SADDLE_POINTS_H
+#define SADDLE_POINTS_H
+
+#include <stdint.h>
+
+typedef struct
+{
+    uint16_t row;
+    uint16_t column;
+} saddle_point_t;
+
+typedef struct
+{
+    uint16_t count;
+    saddle_point_t points[];
+} saddle_points_t;
+
+saddle_points_t *saddle_points(uint16_t row_count, uint16_t row_size, uint8_t matrix[row_count][row_size]);
+
+void free_saddle_points(saddle_points_t *to_free);
+
+#endif
