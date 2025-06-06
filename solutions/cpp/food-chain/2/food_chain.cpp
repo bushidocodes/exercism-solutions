8519e614674744058a9cbadef803e03bcc468276
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:28 +0000

    [Sync Iteration] cpp/food-chain/2

---
+++ solutions/cpp/food-chain/2/food_chain.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+
+#include "food_chain.h"
+
+namespace food_chain
+{
+    std::vector<std::string> creatures{
+        "",
+        "fly",
+        "spider",
+        "bird",
+        "cat",
+        "dog",
+        "goat",
+        "cow",
+        "horse"};
+
+    std::vector<std::string> creatures_antics{
+        "",
+        "",
+        "It wriggled and jiggled and tickled inside her.\n",
+        "How absurd to swallow a bird!\n",
+        "Imagine that, to swallow a cat!\n",
+        "What a hog, to swallow a dog!\n",
+        "Just opened her throat and swallowed a goat!\n",
+        "I don't know how she swallowed a cow!\n"};
+
+    std::string
+    verse(int verse_number)
+    {
+        std::string verse = "I know an old lady who swallowed a " + creatures.at(verse_number) + ".\n";
+
+        if (verse_number >= 2 && verse_number < 8)
+        {
+            verse += creatures_antics.at(verse_number);
+        }
+
+        /* Using Switch Fallthrough because verses accumulate this portion */
+        switch (verse_number)
+        {
+        case 8:
+            verse += "She's dead, of course!\n";
+            break;
+        case 7:
+            verse += "She swallowed the cow to catch the goat.\n";
+        case 6:
+            verse += "She swallowed the goat to catch the dog.\n";
+        case 5:
+            verse += "She swallowed the dog to catch the cat.\n";
+        case 4:
+            verse += "She swallowed the cat to catch the bird.\n";
+        case 3:
+            verse += "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n";
+        case 2:
+            verse += "She swallowed the spider to catch the fly.\n";
+        case 1:
+            verse += "I don't know why she swallowed the fly. Perhaps she'll die.\n";
+        }
+
+        return verse;
+    }
+
+    std::string verses(int start_verse, int end_verse)
+    {
+        std::string verses = "";
+        for (int i = start_verse; i <= end_verse; i++)
+        {
+            verses += verse(i) + "\n";
+        }
+
+        return verses;
+    };
+
+    std::string sing()
+    {
+        return verses(1, 8);
+    };
+
+} // namespace food_chain
