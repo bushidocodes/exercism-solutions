9bd87a80fde161aefa3c6be57dfdeb6d1e789388
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:43 +0000

    [Sync Iteration] c/armstrong-numbers/3

---
+++ solutions/c/armstrong-numbers/3/armstrong_numbers.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "armstrong_numbers.h"
+
+int count_digits(const int source) {
+    int digit_count = 0;
+    for (int buffer = source; buffer > 0; buffer /= 10) {
+        digit_count++;
+    }
+
+    return digit_count;
+}
+
+int calc_candidate_armstrong_number(const int source) {
+    int result = 0;
+    int digit_count = count_digits(source);
+    for (int buffer = source; buffer > 0; buffer /= 10) {
+        result += pow(buffer % 10, digit_count);
+    }
+
+    return result;
+}
+
+int is_armstrong_number(int candidate) {
+    return (candidate == calc_candidate_armstrong_number(candidate));
+}
---
+++ solutions/c/armstrong-numbers/3/armstrong_numbers.h
@@ -0,0 +1,4 @@
+#pragma once
+int count_digits(const int source);
+int calc_candidate_armstrong_number(const int source);
+int is_armstrong_number(int candidate);
