a9acf5ea2d11377c54852171308b6644a456345e
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:51:36 +0000

    [Sync Iteration] javascript/robot-name/2

---
+++ solutions/javascript/robot-name/2/robot-name.js
@@ -0,0 +1,50 @@
+const getRandomLetter = () => {
+  const start = "A".charCodeAt(0);
+  const offset = Math.floor(Math.random() * 26);
+  return String.fromCharCode(start + offset);
+};
+
+const getRandomDigit = () => {
+  return Math.floor(Math.random() * 9).toString();
+};
+
+export class Robot {
+  static existingNames = [];
+  #name = "";
+  static generateName() {
+    return [
+      getRandomLetter(),
+      getRandomLetter(),
+      getRandomDigit(),
+      getRandomDigit(),
+      getRandomDigit(),
+    ].join("");
+  }
+
+  reset() {
+    let candidate;
+    do {
+      candidate = Robot.generateName();
+    } while (Robot.existingNames.includes(candidate));
+
+    Robot.existingNames.push(candidate);
+
+    this.#name = candidate;
+  }
+
+  constructor() {
+    this.reset();
+  }
+
+  get name() {
+    return `${this.#name}`;
+  }
+
+  set name(value) {
+    throw new Error("Name is immutable");
+  }
+}
+
+Robot.releaseNames = () => {
+  Robot.existingNames.reset();
+};
