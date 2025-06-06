0a07b0c3ab726c5ca1df8588052326c346601a51
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:34 +0000

    [Sync Iteration] cpp/scrabble-score/1

---
+++ solutions/cpp/scrabble-score/1/scrabble_score.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cctype>
+#include <cstdint>
+#include <string>
+
+namespace scrabble_score
+{
+    std::uint16_t score(const std::string &word);
+} // namespace scrabble_score
