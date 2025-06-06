c2222ba413460796c597913d369655c5a6f589aa
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:51:50 +0000

    [Sync Iteration] javascript/darts/1

---
+++ solutions/javascript/darts/1/darts.js
@@ -0,0 +1,7 @@
+export const score = (x, y) => {
+  let distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  if (distance > 10) return 0;
+  else if (distance > 5) return 1;
+  else if (distance > 1) return 5;
+  else return 10;
+};
