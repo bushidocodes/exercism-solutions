4ec05d17444f2a064d4b4296341d45bb253db1aa
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:21 +0000

    [Sync Iteration] cpp/hello-world/4

---
+++ solutions/cpp/hello-world/4/hello_world.cpp
@@ -0,0 +1,5 @@
+#include "hello_world.h"
+
+auto hello_world::hello() -> std::string_view {
+    return "Hello, World!";
+}
---
+++ solutions/cpp/hello-world/4/hello_world.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string_view>
+
+namespace hello_world {
+    auto hello() -> std::string_view;
+} // namespace hello_world
