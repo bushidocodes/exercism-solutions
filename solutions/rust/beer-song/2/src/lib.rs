1d37c7bf366623f868c4587d72ab4933783dff26
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:10 +0000

    [Sync Iteration] rust/beer-song/2

---
+++ solutions/rust/beer-song/2/src/lib.rs
@@ -0,0 +1,18 @@
+#![feature(iter_intersperse)]
+
+pub fn verse(n: i32) -> String {
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => format!("{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n, n),
+        2 => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n, n, n - 1),
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n - 1)
+    }
+}
+
+pub fn sing(start: i32, end: i32) -> String {
+    (end..=start)
+        .rev()
+        .map(|num_beer| verse(num_beer))
+        .intersperse("\n".to_string())
+        .collect()
+}
