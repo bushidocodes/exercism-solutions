0830cb2a905132ca696b6dbca168357c0157cca8
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:51:41 +0000

    [Sync Iteration] javascript/run-length-encoding/4

---
+++ solutions/javascript/run-length-encoding/4/run-length-encoding.js
@@ -0,0 +1,36 @@
+export const encode = (plain) => {
+  let cur, curCount;
+  let cipher = "";
+
+  plain.split("").forEach((c, idx, arr) => {
+    if (cur != c) {
+      if (cur) cipher += `${curCount > 1 ? curCount : ""}${cur}`;
+      cur = c;
+      curCount = 1;
+    } else {
+      curCount++;
+    }
+
+    // If last char dump
+    if (idx == arr.length - 1) {
+      cipher += `${curCount > 1 ? curCount : ""}${cur}`;
+    }
+  });
+  return cipher;
+};
+
+export const decode = (cipher) => {
+  const runLengthRegex = /([0-9]*)?(.)(.+)?/;
+  let plain = "";
+  for (
+    let buffer = cipher.match(runLengthRegex);
+    !!buffer;
+    buffer = buffer.match(runLengthRegex)
+  ) {
+    let [, count = 1, char = "", rest = ""] = buffer;
+    buffer = rest;
+    for (let i = 0; i < count; i++) plain += char;
+  }
+
+  return plain;
+};
