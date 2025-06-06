d3bf6ac92c2ccb76b1ac7bc0369b3dd3ea6c6ee3
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:25 +0000

    [Sync Iteration] c/binary-search/2

---
+++ solutions/c/binary-search/2/src/binary_search.c
@@ -0,0 +1,18 @@
+#include "binary_search.h"
+
+int *binary_search(int value, const int *arr, size_t length)
+{
+    if (length == 0)
+        return NULL;
+
+    int middle = length / 2;
+    if (value == *(arr + middle))
+        return (int *)(arr + middle);
+    else if (middle == 0)
+        return NULL;
+    else if (value < *(arr + middle))
+        return binary_search(value, arr, middle);
+    else if (value > *(arr + middle))
+        return binary_search(value, arr + middle, length - middle);
+    return NULL;
+}
