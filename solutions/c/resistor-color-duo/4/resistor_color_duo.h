a06ef4649e5004a681a42e7b56605c17420831ff
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:47 +0000

    [Sync Iteration] c/resistor-color-duo/4

---
+++ solutions/c/resistor-color-duo/4/resistor_color_duo.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typedef enum resistor_band_t {
+	BLACK,
+	BROWN,
+	RED,
+	ORANGE,
+	YELLOW,
+	GREEN,
+	BLUE,
+	VIOLET,
+	GREY,
+	WHITE,
+	RESISTOR_BAND_COUNT
+} resistor_band_t;
+
+int color_code(resistor_band_t color[static 2]);
