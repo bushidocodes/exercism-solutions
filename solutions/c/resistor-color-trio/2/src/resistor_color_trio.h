0efe6a1a223eda7448c8a4388a09e918447caf78
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:47 +0000

    [Sync Iteration] c/resistor-color-trio/2

---
+++ solutions/c/resistor-color-trio/2/src/resistor_color_trio.h
@@ -0,0 +1,30 @@
+#ifndef RESISTOR_COLOR_TRIO_H
+#define RESISTOR_COLOR_TRIO_H
+
+typedef enum resistor_band_t
+{
+    BLACK,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE
+} resistor_band_t;
+
+typedef struct
+{
+    unsigned short value;
+    enum
+    {
+        OHMS,
+        KILOOHMS
+    } unit;
+} resistor_value_t;
+
+resistor_value_t color_code(resistor_band_t color[]);
+
+#endif
