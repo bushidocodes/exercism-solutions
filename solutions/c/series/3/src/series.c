f776a060590efbbba885a69cb5827f42c4ef5590
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:28 +0000

    [Sync Iteration] c/series/3

---
+++ solutions/c/series/3/src/series.c
@@ -0,0 +1,31 @@
+#include "series.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+series_results_t series(char *input_text, unsigned int substring_length)
+{
+    size_t input_length = strlen(input_text);
+
+    bool is_valid = input_length <= MAX_INPUT_TEXT_LENGTH && substring_length > 0 && substring_length <= input_length && substring_length <= MAX_SERIES_LENGTH;
+
+    // TODO: There's a memory leak here?
+    series_results_t result = {
+        .substring_count = is_valid ? input_length - (substring_length - 1) : 0,
+        .substring = calloc(sizeof(char *), result.substring_count)};
+
+    if (result.substring == NULL)
+        exit(EXIT_FAILURE);
+
+    for (size_t i = 0; i < result.substring_count; i++)
+    {
+        // Pad for \n
+        result.substring[i] = calloc(sizeof(char), substring_length + 1);
+        if (result.substring[i] == NULL)
+            exit(EXIT_FAILURE);
+        strncpy(result.substring[i], &input_text[i], substring_length);
+    }
+
+    return result;
+}
