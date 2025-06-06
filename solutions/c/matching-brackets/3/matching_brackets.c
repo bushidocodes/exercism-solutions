fab87d3ef86d569c315af666981a3e42afc1f12e
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:36 +0000

    [Sync Iteration] c/matching-brackets/3

---
+++ solutions/c/matching-brackets/3/matching_brackets.c
@@ -0,0 +1,44 @@
+#include "matching_brackets.h"
+
+#include <stdio.h>
+#include <string.h>
+#define MAX_STACK 50
+
+bool is_paired(const char *input) {
+	if (input == NULL) return false;
+
+	// Simple Stack. If top is -1, stack is empty
+	char stack[MAX_STACK];
+	int stack_top = -1;
+
+	for (unsigned int i = 0; i < strlen(input); i++) {
+		switch (input[i]) {
+			case '[':
+			case '{':
+			case '(':
+				stack[++stack_top] = input[i];
+				break;
+			case ']':
+				if (stack[stack_top] != '[') {
+					return false;
+				}
+				stack_top--;
+				break;
+			case ')':
+				if (stack[stack_top] != '(') {
+					return false;
+				}
+				stack_top--;
+				break;
+			case '}':
+				if (stack[stack_top] != '{') {
+					return false;
+				}
+				stack_top--;
+				break;
+			default:
+				break;
+		}
+	}
+	return stack_top == -1;
+}
