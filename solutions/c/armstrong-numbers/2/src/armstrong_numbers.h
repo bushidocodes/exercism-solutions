3ab8889eb1378170e269e4b536bbc82e6e7857c3
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:43 +0000

    [Sync Iteration] c/armstrong-numbers/2

---
+++ solutions/c/armstrong-numbers/2/src/armstrong_numbers.h
@@ -0,0 +1,8 @@
+#ifndef ARMSTRONG_NUMBERS
+#define ARMSTRONG_NUMBERS
+
+int count_digits(const int source);
+int calc_candidate_armstrong_number(const int source);
+int is_armstrong_number(int candidate);
+
+#endif
