2307a985f4c9e7771a8c25dc724ac3637acd0540
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:51:07 +0000

    [Sync Iteration] bash/series/2

---
+++ solutions/bash/series/2/series.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+panic() {
+	(($# == 1)) && printf "%s\n" "$1"
+	exit 1
+}
+
+# Breaks the series of ASCII characters $1 into slices of length $2 delimited by a space
+series_slice() {
+	(($# != 2)) && panic "Invalid Arg Count. Saw $#. Expected 2"
+
+	local -r series="$1"
+	local -r series_len="${#series}"
+	local -ri slice_len="$2"
+
+	((series_len == 0)) && panic "series cannot be empty"
+	((slice_len > series_len)) && panic "slice length cannot be greater than series length"
+	((slice_len == 0)) && panic "slice length cannot be zero"
+	((slice_len < 0)) && panic "slice length cannot be negative"
+
+	local -a slices=()
+	for ((i = 0; i + slice_len <= series_len; i++)); do
+		slices+=("${series:i:slice_len}")
+	done
+
+	printf "%s\n" "${slices[*]}"
+
+	return 0
+}
+
+series_slice "$@"
