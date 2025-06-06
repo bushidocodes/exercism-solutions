ed104903ff4aac6f4ea19ebeb81a45015571262d
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:27 +0000

    [Sync Iteration] c/allergies/1

---
+++ solutions/c/allergies/1/src/allergies.c
@@ -0,0 +1,26 @@
+#include "allergies.h"
+#include <stdio.h>
+
+bool is_allergic_to(allergen_t allergen, int score)
+{
+    return ((1 << allergen) & score) > 0;
+}
+
+allergen_list_t get_allergens(int score)
+{
+    allergen_list_t results;
+    results.count = 0;
+
+    for (allergen_t allergen = ALLERGEN_EGGS; allergen < ALLERGEN_COUNT; allergen++)
+    {
+        if (is_allergic_to(allergen, score))
+        {
+            results.allergens[allergen] = true;
+            results.count++;
+        }
+        else
+            results.allergens[allergen] = false;
+    }
+
+    return results;
+}
