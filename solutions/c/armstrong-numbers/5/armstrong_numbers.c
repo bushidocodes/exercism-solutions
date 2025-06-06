8b5f2d50cc122461f00054a6be4119ddde5fa150
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:43 +0000

    [Sync Iteration] c/armstrong-numbers/5

---
+++ solutions/c/armstrong-numbers/5/armstrong_numbers.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include "armstrong_numbers.h"
+
+static inline int count_digits(const int source) {
+    int digit_count = 0;
+    for (int buffer = source; buffer > 0; buffer /= 10) {
+        digit_count++;
+    }
+
+    return digit_count;
+}
+
+static inline int sum_digits_raised_to_power_of_digit_count(const int source) {
+    int result = 0;
+    int digit_count = count_digits(source);
+    for (int buffer = source; buffer > 0; buffer /= 10) {
+        result += pow(buffer % 10, digit_count);
+    }
+
+    return result;
+}
+
+bool is_armstrong_number(int candidate) {
+    return sum_digits_raised_to_power_of_digit_count(candidate) == candidate;
+}
