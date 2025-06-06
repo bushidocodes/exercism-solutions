2b385a7f864f657878c45e8483a73c4888a32ee5
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:51:10 +0000

    [Sync Iteration] bash/sublist/2

---
+++ solutions/bash/sublist/2/sublist.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Encodes a list as comma separted elements with a trailing comma
+stringify() {
+	local -r raw_list="$1"
+	local -n __string="$2"
+	IFS="[], "
+	set -o noglob
+	for element in $raw_list; do
+		[[ -n "$element" ]] && __string+="$element,"
+	done
+	set +o noglob
+	return 0
+}
+
+# Once strinified, glob to check for sublists
+main() {
+	local first_list=""
+	stringify "$1" first_list || exit 1
+	stringify "$2" second_list || exit 1
+
+	if [[ "$first_list" == "$second_list" ]]; then
+		printf "equal\n"
+	elif [[ "$first_list" == *$second_list* ]]; then
+		printf "superlist\n"
+	elif [[ "$second_list" == *$first_list* ]]; then
+		printf "sublist\n"
+	else
+		printf "unequal\n"
+	fi
+
+	return 0
+}
+
+main "$@"
