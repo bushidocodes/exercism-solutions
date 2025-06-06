0ecadcb4704bca419cbd9723cb7036c3800358e9
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:51:12 +0000

    [Sync Iteration] bash/resistor-color-trio/2

---
+++ solutions/bash/resistor-color-trio/2/resistor_color_trio.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+get_resistance_value () {
+  declare -n color=$1
+
+  local value
+
+  case $color in
+    black | Black)
+      value="0"
+      ;;
+    brown | Brown)
+      value="1"
+      ;;
+    red | Red)
+      value="2"
+      ;;
+    orange | Orange)
+      value="3"
+      ;;
+    yellow | Yellow)
+      value="4"
+      ;;
+    green | Green)
+      value="5"
+      ;;
+    blue | Blue)
+      value="6"
+      ;;
+    violet | Violet)
+      value="7"
+      ;;
+    grey | Grey | gray | Gray)
+      value="8"
+      ;;
+    white | White)
+      value="9"
+      ;;
+    *)
+      value="err"
+      ;;
+  esac
+
+  printf "$value"
+}
+
+main () {
+  declare -n first_color=$1; declare -n second_color=$2; declare -n third_color=$3
+
+  local first_value=$(get_resistance_value $first_color)
+  local second_value=$(get_resistance_value $second_color)
+  local third_value=$(get_resistance_value $third_color)
+
+  if [[ "$first_value" = "err" ]] || [[ "$second_value" = "err" ]] || [[ "$third_value" = "err" ]]; then
+    printf "invalid color\n"
+    return 1
+  fi
+
+  local result="$(( ($first_value * 10 + $second_value) * (10 ** $third_value) ))"
+
+  if [[ $result -eq 0 ]]; then
+    printf "$result ohms\n"
+  elif [[ $(("$result" % 1000000000)) -eq 0 ]]; then
+    (( result /= 1000000000 ))
+    printf "%d gigaohms\n" "$(( $result ))"
+  elif [[ $(("$result" % 1000000)) -eq 0 ]]; then
+    (( result /= 1000000 ))
+    printf "%d megaohms\n" "$(( $result ))"
+  elif [[ $(("$result" % 1000)) -eq 0 ]]; then
+    (( result /= 1000 ))
+    printf "%d kiloohms\n" "$(( $result ))"
+  else
+    printf "$result ohms\n"
+  fi
+  return 0;
+}
+
+main "$@"
