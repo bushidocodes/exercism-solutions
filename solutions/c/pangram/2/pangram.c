a6a723a0b9a815931e85fb8d02b9f3a0867c1356
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:20 +0000

    [Sync Iteration] c/pangram/2

---
+++ solutions/c/pangram/2/pangram.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include "pangram.h"
+
+#define BASE_ALPHA 65
+
+bool is_pangram(const char *sentence)
+{
+    if (sentence == NULL) return false;
+
+    int letter_count[26] = {0};
+
+    for (unsigned int i = 0; i < strlen(sentence); i++) {
+        if (isalpha(sentence[i])) letter_count[toupper(sentence[i]) - BASE_ALPHA]++;
+    }
+
+    for (unsigned int i = 0; i < 26; i++) {
+        if (letter_count[i] == 0) return false;
+    }
+
+    return true;
+}
