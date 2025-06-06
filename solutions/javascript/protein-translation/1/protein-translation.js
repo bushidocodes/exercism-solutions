bdcf0373c74faabfd7098f6478fbeb23479ac929
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:51:49 +0000

    [Sync Iteration] javascript/protein-translation/1

---
+++ solutions/javascript/protein-translation/1/protein-translation.js
@@ -0,0 +1,33 @@
+const codonProteinMapping = {
+  AUG: "Methionine",
+  UUU: "Phenylalanine",
+  UUC: "Phenylalanine",
+  UUA: "Leucine",
+  UUG: "Leucine",
+  UCU: "Serine",
+  UCC: "Serine",
+  UCA: "Serine",
+  UCG: "Serine",
+  UAU: "Tyrosine",
+  UAC: "Tyrosine",
+  UGU: "Cysteine",
+  UGC: "Cysteine",
+  UGG: "Tryptophan",
+  UAA: "STOP",
+  UAG: "STOP",
+  UGA: "STOP",
+};
+
+export const translate = (codonString) => {
+  if (!codonString) return [];
+
+  const codons = [];
+  for (let i = 0; i < codonString.length; i += 3) {
+    let codon = codonString.substring(i, i + 3);
+    if (codonProteinMapping[codon] == "STOP") break;
+    if (!codonProteinMapping[codon]) throw new Error("Invalid codon");
+    codons.push(codonString.substring(i, i + 3));
+  }
+
+  return codons.map((codon) => codonProteinMapping[codon]);
+};
