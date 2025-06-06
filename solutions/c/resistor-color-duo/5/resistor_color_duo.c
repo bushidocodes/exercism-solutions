fb7ad1d0abe09d907ecc6bd0fc4e09117a12625b
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:47 +0000

    [Sync Iteration] c/resistor-color-duo/5

---
+++ solutions/c/resistor-color-duo/5/resistor_color_duo.c
@@ -0,0 +1,11 @@
+#include "resistor_color_duo.h"
+#include <assert.h>
+
+int color_code(const resistor_band_t color[static 2]) {
+    assert(color[0] >= 0);
+    assert(color[0] < RESISTOR_BAND_COUNT);
+    assert(color[1] >= 0);
+    assert(color[1] < RESISTOR_BAND_COUNT);
+    
+	return 10 * color[0] + color[1];
+}
