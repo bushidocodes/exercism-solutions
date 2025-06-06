c4ad61f804222dbb983b4e8c2b14f837028cab90
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:51:02 +0000

    [Sync Iteration] bash/all-your-base/1

---
+++ solutions/bash/all-your-base/1/all_your_base.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+main () {
+    declare -i sourceRadix=$1;
+    declare source=$2;
+    declare -i targetRadix=$3;
+
+    # Validate Radices
+    ((sourceRadix < 2)) && {
+        echo "sourceRadix must be 2 or higher";
+        return 1;
+    }
+    ((targetRadix < 2)) && {
+        echo "targetRadix must be 2 or higher";
+        return 1;
+    }
+
+    # Split source string into an array of digits
+    declare -a sourceDigits=();
+    OIFS=$IFS;
+    IFS=' ';
+    for digit in $source; do
+        sourceDigits+=("$digit");
+    done
+
+    # Calculate decimal value
+    declare -i decimalBuffer=0;
+    for ((i = 0; i < ${#sourceDigits[@]}; i++)); do
+        if ((${sourceDigits[i]} < 0 || ${sourceDigits[i]} >= sourceRadix)); then
+            echo "digit must be between 0 and sourceRadix - 1";
+            return 1;
+        fi
+        ((decimalBuffer = decimalBuffer * sourceRadix + ${sourceDigits[i]}))
+    done
+
+    # Convert decimal value to target radix, with digits reverse
+    declare -a targetDigitsReversed=();
+    while ((decimalBuffer > 0)); do
+        targetDigitsReversed+=("$((decimalBuffer % targetRadix))");
+        ((decimalBuffer /= targetRadix));
+    done
+
+    # Reverse digits and join using spaces as delimiter
+    declare -a targetDigits=();
+    for ((i = ${#targetDigitsReversed[@]}; i >= 0; i--)); do
+        targetDigits+=(${targetDigitsReversed[i]})
+    done
+
+    echo "${targetDigits[*]}";
+    IFS=$OIFS;
+}
+
+main "$@"
