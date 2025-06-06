47cb527d610269f8605e11b17a4b536e3f0eeba6
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:30 +0000

    [Sync Iteration] c/sieve/3

---
+++ solutions/c/sieve/3/sieve.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+/// Calculate at most `max_primes` prime numbers in the interval [2,limit]
+/// using the Sieve of Eratosthenes and store the prime numbers in `primes`
+/// in increasing order.
+/// The function returns the number of calculate primes.
+extern uint32_t sieve(uint32_t limit, uint32_t *primes, size_t primes_capacity);
