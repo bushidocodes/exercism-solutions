07cafd3340010bbf3b7fb036f547daf1849ba7d1
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:22 +0000

    [Sync Iteration] c/rna-transcription/4

---
+++ solutions/c/rna-transcription/4/rna_transcription.c
@@ -0,0 +1,40 @@
+#include "rna_transcription.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Cannot use POSIX calls on Exercism
+// #define _POSIX_C_SOURCE = 200809L
+
+char *to_rna(const char *dna) {
+	// strndup will be in C2X as well
+#ifdef _POSIX_C_SOURCE
+	char *buffer = strndup(dna, strlen(dna));
+#else
+	size_t dna_len = strlen(dna) + 1;
+	char *buffer = calloc(dna_len, sizeof(char));
+	strncpy(buffer, dna, dna_len);
+#endif
+
+	for (char *c = buffer; *c != '\0'; c++) {
+		switch (*c) {
+			case 'G':
+				*c = 'C';
+				continue;
+			case 'C':
+				*c = 'G';
+				continue;
+			case 'T':
+				*c = 'A';
+				continue;
+			case 'A':
+				*c = 'U';
+				continue;
+			default:
+				return NULL;
+		}
+	}
+
+	return buffer;
+}
