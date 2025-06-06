6002d95d6f44bcb440e0297bf411096a212e9787
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:37 +0000

    [Sync Iteration] cpp/grains/4

---
+++ solutions/cpp/grains/4/grains.cpp
@@ -0,0 +1,37 @@
+#include "grains.h"
+
+#include <stdexcept>
+
+namespace grains {
+const std::uint64_t CHESSBOARD_SQUARE_COUNT = 64;
+
+std::uint64_t square(std::uint64_t chessboard_square) {
+	if (chessboard_square == 0 || chessboard_square > CHESSBOARD_SQUARE_COUNT)
+		throw std::invalid_argument{"Must be a square between 1 and 64"};
+
+	/*
+	 * Because computers use a base-2 number system, shifting the digits left
+	 * one position is equivalent to multiplying by two
+	 *
+	 * Thus
+	 * 0001 = 1
+	 * 0010 = 2
+	 * 0100 = 4
+	 * 1000 = 8
+	 *
+	 * This allows the chessboard to be modeled as a 64 element bit array
+	 */
+	return 1ULL << (chessboard_square - 1);
+}
+
+std::uint64_t total() {
+	/*
+	 * Because grains double like the base-2 number system and a chessboard
+	 * is the same size as a 64-bit unsigned integer, the total value is 64
+	 * consecutive 1s or 0xFFFFFFFFFFFFFFFF. In cstdint or stdint.h, this is
+	 * also expressed as the macro UINT64_MAX
+	 */
+	return UINT64_MAX;
+}
+
+}  // namespace grains
---
+++ solutions/cpp/grains/4/grains.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+namespace grains {
+
+std::uint64_t square(std::uint64_t input);
+std::uint64_t total();
+
+}  // namespace grains
