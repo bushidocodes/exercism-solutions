8cdd36a965f7903b95a84c637449a6d2f09e79fc
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:50:27 +0000

    [Sync Iteration] c/atbash-cipher/1

---
+++ solutions/c/atbash-cipher/1/src/atbash_cipher.c
@@ -0,0 +1,64 @@
+#include "atbash_cipher.h"
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+static inline char encode(char source)
+{
+    return 'z' - (tolower(source) - 'a');
+}
+
+static inline char decode(char source)
+{
+    return 'a' + ('z' - source);
+}
+
+char *atbash_encode(const char *input)
+{
+    size_t input_length = strlen(input);
+    char *result = calloc(input_length + 1, sizeof(char));
+    size_t result_length = 0;
+
+    for (size_t i = 0; i < input_length; i++)
+    {
+        if (isalpha(input[i]))
+        {
+            result[result_length++] = encode(input[i]);
+        }
+        else if (isdigit(input[i]))
+        {
+
+            result[result_length++] = input[i];
+        }
+        if ((result_length + 1) % 6 == 0)
+        {
+            // Need to scan forward to the end of the string to make sure we're not the last letter
+
+            bool is_last_letter = true;
+            for (size_t k = i + 1; k < input_length; k++)
+            {
+                if (isalnum(input[k]))
+                    is_last_letter = false;
+            }
+
+            if (!is_last_letter)
+                result[result_length++] = ' ';
+        }
+    }
+    result[result_length++] = '\0';
+    return result;
+}
+char *atbash_decode(const char *input)
+{
+    size_t input_length = strlen(input);
+    char *result = calloc(input_length + 1, sizeof(char));
+    size_t result_length = 0;
+
+    for (size_t i = 0; i < input_length; i++)
+    {
+        result[result_length++] = decode(input[i]);
+    }
+    result[result_length++] = '\0';
+    return result;
+}
