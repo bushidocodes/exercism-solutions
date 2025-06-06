b3b0750c4c46ddb29cf351b41fb283c680cf0cd7
root <root@ip-10-1-0-225.eu-west-2.compute.internal>
2025-06-06 05:52:43 +0000

    [Sync Iteration] cpp/rna-transcription/2

---
+++ solutions/cpp/rna-transcription/2/rna_transcription.cpp
@@ -0,0 +1,34 @@
+#include <map>
+
+#include "rna_transcription.h"
+
+namespace rna_transcription
+{
+    std::map<char, char> transcription_pairs = {{'C', 'G'}, {'G', 'C'}, {'A', 'U'}, {'T', 'A'}};
+
+    void validate_dna_nucleotide(char nucleotide)
+    {
+        if (nucleotide != 'A' && nucleotide != 'C' && nucleotide != 'G' && nucleotide != 'T')
+        {
+            throw std::invalid_argument{"Character is not a valid nucleotide"};
+        };
+    }
+
+    char to_rna(char nucleotide)
+    {
+        validate_dna_nucleotide(nucleotide);
+        return transcription_pairs.at(nucleotide);
+    };
+
+    std::string to_rna(std::string dna)
+    {
+        std::string rna = "";
+        for (char nucleotide : dna)
+        {
+            rna += to_rna(nucleotide);
+        }
+
+        return rna;
+    }
+
+} // namespace rna_transcription
